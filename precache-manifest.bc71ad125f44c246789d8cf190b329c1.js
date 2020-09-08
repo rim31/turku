f722e59326d2dec24bad18fd5767a9d7b4b16d3b
toto42 <toto42@yopmail.com>
2020-09-08 22:34:19 +0300

    1rst push

---
+++ precache-manifest.bc71ad125f44c246789d8cf190b329c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c78354d2950a644df859880a8cd896c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f392fbf1a2f935ec4d",
+    "url": "/static/css/main.fd94893c.chunk.css"
+  },
+  {
+    "revision": "c839ad55d551a972342d",
+    "url": "/static/js/2.cd6d6c08.chunk.js"
+  },
+  {
+    "revision": "7dbdbed09d75c149fb064b7d7dc6df24",
+    "url": "/static/js/2.cd6d6c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f392fbf1a2f935ec4d",
+    "url": "/static/js/main.1d6871fa.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "ca8e92096ce3c9f2d221de8a645c0010",
+    "url": "/static/media/map.ca8e9209.png"
+  }
+]);
